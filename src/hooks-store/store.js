c40efbe344de857477babb87766b33449f3c1772
florianbar <florianbar01@gmail.com>
2020-09-21 15:59:51 +0200

    Added custom hook store

---
+++ src/hooks-store/store.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+export const useStore = () => {
+    const setState = useState(globalState)[1];
+
+    const dispatch = actionIdentifier => {
+        const newState = actions[actionIdentifier](globalState);
+        globalState = {
+            ...globalState, 
+            ...newState
+        };
+
+        for (const listener of listeners) {
+            listener(globalState);
+        }
+    };
+
+    useEffect(() => {
+        listeners.push(setState);
+
+        // Clean-up function
+        return () => {
+            listeners = listeners.filter(item=> item !== setState);
+        }
+    }, [setState]);
+
+    return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+    if (initialState) {
+        globalState = {...globalState, ...initialState};
+    }
+    actions = {...actions, ...userActions};
+};
